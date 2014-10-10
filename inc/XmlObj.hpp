a2d34e8e726cac07f34563add2d17ac93a1dedeb
jaccar_a <jaccar_a@epitech.eu>
2014-10-10 17:01:26 +0200

    update indentation

--- inc/XmlObj.hpp
+++ inc/XmlObj.hpp
@@ -1,9 +1,9 @@
 //
 // XmlObj.hpp for XmlObj in /home/jaccar_a/Documents/ADC
-// 
+//
 // Made by Quentin Jaccarino
 // Login   <jaccar_a@localhost.localdomain>
-// 
+//
 // Started on  Wed Oct  1 20:29:56 2014 Quentin Jaccarino
 // Last update Thu Oct  2 01:55:30 2014 Quentin Jaccarino
 //
@@ -12,17 +12,17 @@
 # define XMLOBJ_HPP_
 
 class	XmlObj {
-public:
-  XmlObj(const string &filename);
-  ~XmlObj();
+    public:
+        XmlObj(const string &filename);
+        ~XmlObj();
 
-  Initialize();
+        Initialize();
 
-private:
-  string filename;
-  std::vector<XmlObj*> need;
-  TestObj tests;
-  LessonObj lesson;
+    private:
+        string filename;
+        std::vector<XmlObj*> need;
+        TestObj tests;
+        LessonObj lesson;
 };
 
 #endif /* !XMLOBJ_HPP_ */
